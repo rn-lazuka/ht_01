4b9f1bc3ebf740124376eba4aca6e1f9655c0728
rn-lazuka <rn.lazuka@gmail.com>
2023-03-30 22:38:34 +0300

    Added routes

---
+++ src/routes/video-router.ts
@@ -0,0 +1,30 @@
+import {Router} from 'express';
+
+const videoRouter = Router()
+
+enum VideoResolutions {
+    P144 = 'P144',
+    P240 = 'P240',
+    P360 = 'P360',
+    P480 = 'P480',
+    P720 = 'P720',
+    P1080 = 'P1080',
+    P1440 = 'P1440',
+    P2160 = 'P2160'
+}
+
+interface Video {
+    id: number;
+    title: string;
+    author: string;
+    canBeDownloaded: boolean;
+    minAgeRestriction: number | null;
+    createdAt: string;
+    publicationDate: string;
+    availableResolutions: VideoResolutions[]
+}
+
+const videos:Video[] = []
+videoRouter.get('/', (req, res) => {
+    res.send(videos)
+})
