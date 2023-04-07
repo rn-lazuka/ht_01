6e237f8e605dbd85bc83f4c8b460ad642f6a55aa
Roman Lazuka <rn.lazuka@gmail.com>
2023-04-07 10:14:28 +0300

    fixed validators

--- src/routes/blogs-router.ts
+++ src/routes/blogs-router.ts
@@ -6,9 +6,9 @@ import {checkAuth} from '../utils';
 export const blogRouter = Router();
 
 const blogValidations = [
-    body('name').isString().withMessage('Name must be a string').isLength({max: 15}).withMessage('Name must not exceed 15 characters'),
-    body('description').isString().withMessage('Description must be a string').isLength({max: 500}).withMessage('Description must not exceed 500 characters'),
-    body('websiteUrl').isString().withMessage('Website URL must be a string').trim().isLength({max: 100}).withMessage('Description must not exceed 100 characters')
+    body('name').isString().withMessage('Name must be a string').trim().notEmpty().isLength({max: 15}).withMessage('Name must not exceed 15 characters'),
+    body('description').isString().withMessage('Description must be a string').trim().notEmpty().isLength({max: 500}).withMessage('Description must not exceed 500 characters'),
+    body('websiteUrl').isString().withMessage('Website URL must be a string').trim().notEmpty().isLength({max: 100}).withMessage('Description must not exceed 100 characters')
         .matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/).withMessage('Website URL must be a valid URL with https protocol'),
 ];
 blogRouter.get('/', (req, res) => {
--- src/routes/posts-router.ts
+++ src/routes/posts-router.ts
@@ -7,9 +7,9 @@ import {checkAuth} from '../utils';
 export const postsRouter = Router();
 
 const postsValidations = [
-    body('title').isString().withMessage('Name must be a string').trim().isLength({max: 30}).withMessage('Name must not exceed 30 characters'),
-    body('shortDescription').isString().withMessage('Name must be a string').trim().isLength({max: 100}).withMessage('Name must not exceed 100 characters'),
-    body('content').isString().withMessage('Description must be a string').trim().isLength({max: 1000}).withMessage('Description must not exceed 1000 characters'),
+    body('title').isString().withMessage('Name must be a string').trim().notEmpty().isLength({max: 30}).withMessage('Name must not exceed 30 characters'),
+    body('shortDescription').isString().withMessage('Name must be a string').trim().notEmpty().isLength({max: 100}).withMessage('Name must not exceed 100 characters'),
+    body('content').isString().withMessage('Description must be a string').trim().notEmpty().isLength({max: 1000}).withMessage('Description must not exceed 1000 characters'),
     body('blogId').custom((value) => {
         const blog = blogRepository.getBlogById(value);
         if (!blog) {
