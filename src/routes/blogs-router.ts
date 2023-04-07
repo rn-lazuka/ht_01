58753f3d3203b53be057f6d1670b0add1f104a61
Roman Lazuka <rn.lazuka@gmail.com>
2023-04-07 09:48:59 +0300

    fixed blogs and posts router

--- src/routes/blogs-router.ts
+++ src/routes/blogs-router.ts
@@ -8,17 +8,18 @@ export const blogRouter = Router();
 const blogValidations = [
     body('name').isString().withMessage('Name must be a string').isLength({max: 15}).withMessage('Name must not exceed 15 characters'),
     body('description').isString().withMessage('Description must be a string').isLength({max: 500}).withMessage('Description must not exceed 500 characters'),
-    body('websiteUrl').isString().withMessage('Website URL must be a string').matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/).withMessage('Website URL must be a valid URL with https protocol'),
+    body('websiteUrl').isString().withMessage('Website URL must be a string')
+        .matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/).withMessage('Website URL must be a valid URL with https protocol'),
 ];
 blogRouter.get('/', (req, res) => {
     const blogs = blogRepository.getBlogs();
-    res.send(blogs);
+    res.json(blogs);
 });
 
 blogRouter.get('/:id', (req, res) => {
     const blog = blogRepository.getBlogById(req.params.id);
     if (blog) {
-        res.send(blog);
+        res.json(blog);
     } else {
         res.sendStatus(404);
     }
@@ -34,7 +35,7 @@ blogRouter.post('/', checkAuth, blogValidations, (req: Request, res: Response) =
         return res.status(400).json({errorMessages});
     }
     const blog = blogRepository.createBlog(req.body);
-    return res.status(201).json({blog});
+    return res.status(201).json(blog);
 });
 blogRouter.put('/:id', checkAuth,[...blogValidations, check('id').notEmpty().withMessage('ID parameter is required'),], (req: Request, res: Response) => {
     const errors = validationResult(req);
