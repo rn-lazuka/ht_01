aa2c03df466e408cab802e12afb9cd471a3ef7fb
Roman Lazuka <rn.lazuka@gmail.com>
2023-05-01 15:20:24 +0300

    ht_05: fixed users validations

--- src/validators/users.ts
+++ src/validators/users.ts
@@ -2,6 +2,6 @@ import {body} from 'express-validator';
 
 export const userValidations = [
     body('login').isString().withMessage('Name must be a string').trim().notEmpty().isLength({max: 10, min: 3}).withMessage('Min 3, max 10 chars').matches(/^[a-zA-Z0-9_-]*$/),
-    body('email').isString().withMessage('Name must be a string').trim().notEmpty().matches(/^[a-zA-Z0-9_-]*$/),
-    body('password').isString().withMessage('Name must be a string').trim().notEmpty().isLength({max: 20, min: 6}).withMessage('Min 6, max 20 chars').matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/),
+    body('email').isString().withMessage('Name must be a string').trim().notEmpty().matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/),
+    body('password').isString().withMessage('Name must be a string').trim().notEmpty().isLength({max: 20, min: 6}).withMessage('Min 6, max 20 chars'),
 ];
