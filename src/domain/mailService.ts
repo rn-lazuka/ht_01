52769fa28cc505cc682ee89a01405b64f657093a
Roman Lazuka <rn.lazuka@gmail.com>
2023-05-22 13:28:53 +0300

    ht_07: fixed confirmation

--- src/domain/mailService.ts
+++ src/domain/mailService.ts
@@ -4,7 +4,7 @@ import {emailAdapter} from '../adapters/emailAdapter';
 export const mailService = {
     async sendEmailConfirmationCode(user: UserEntity) {
         const mail = `<div><h1>Thank for your registration</h1><p>To finish registration please follow the link below:
-        <a href=\`https://somesite.com/confirm-email?code=${user.emailConfirmation?.confirmationCode}\`>complete registration</a></p></div>`;
+        <a href="https://somesite.com/confirm-email?code=${user.emailConfirmation?.confirmationCode}">complete registration</a></p></div>`;
         const info = await emailAdapter.sendMail(user.email, 'Confirmation code', mail);
         return info ? info : null;
     }
